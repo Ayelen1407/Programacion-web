c63ec98241112c4b383751cecad9cbc66b31d037
Ayelen Quispe <quispeaguilar.bel3n@gmail.com>
2025-09-03 17:03:47 -0300

    arregle unas cosas de la estructura del codigo

--- HTML-CSS/React/prev-state-spread-operator/src/App.jsx
+++ HTML-CSS/React/prev-state-spread-operator/src/App.jsx
@@ -7,27 +7,23 @@ function App() {
  const [persona, setPersona] = useState({nombre: "Pepito", edad: 30})
 //Spread Operator -> se agregan tres puntos antes de la variable que incluye
 //todos los objetos ya existentes y despues de la coma se pone lo que queremos agregar
+
+//TAREA1: Hacer que el setNum agregue numeros consecutivos -> 3p
 function myFunction (){
  setNum([...num, num.length+1])
 }
 
-
-function nuevaPropiedad (){
-  const dataPersona = ({...persona, apellido: "Diaz"})
-
-
- setPersona(dataPersona)
- console.log(dataPersona) //terminar
-}
-//TAREA1: Hacer que el setNum agregue numeros consecutivos -> 3p OK
 //TAREA2: Agregar una propiedad al objeto persona usando spread operator -> 4p
+function nuevaPropiedad (){
+  const datoP = ({...persona, apellido: "Diaz"})
+  setPersona(datoP)
+  console.log(datoP)
+}
+
 //TAREA3  partiendo de {nombre: "pepito", edad: 30} cambia solo el nombre a "Luis" usando spread ->3p
-
-
  const cambioNombre = () => {
-   setPersona({...persona, nombre: "Luis"})
+   setPersona ({...persona, nombre: "Luis"})
  }
-
 
 return (
    <>
@@ -35,17 +31,14 @@ return (
      <p>nombre: {persona.nombre}</p>
      <p>edad: {persona.edad}</p>
      <button onClick={cambioNombre}>Cambiar a Luis</button>
-   </div>
-    <div>
+
      {num.map((item, index) => (
        <p>{item}</p>
      ))}
+     
      <button onClick={myFunction}>Agregar numero</button>
-    </div>
-    <div>
      <button onClick={nuevaPropiedad}>Agregar propiedad</button>
     </div>
-
 
    </>
  )
